91b332b4dee7b2d4b26c9d1ab98179e2ec238978
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-04-30 22:27:52 +0200

    Add missing icons

---
+++ app/src/components/icons/EllipsisVerticalIcon.tsx
@@ -0,0 +1,9 @@
+import type { Component } from 'solid-js'
+
+const EllipsisVerticalIcon: Component<{ class?: string }> = (props) => (
+  <svg class={props.class ?? 'w-5 h-5'} fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+  </svg>
+)
+
+export default EllipsisVerticalIcon
